7e6bc48889aad8fc87631ea0509a5916f8eeb7b4
Domzori <noahhoffer@gmail.com>
2021-07-14 21:19:24 -0700

    fixed spelling error and StatusData now functions as intended

--- src/data/scripts/shipsystems/LunaBurnDriveStats.java
+++ src/data/scripts/shipsystems/LunaBurnDriveStats.java
@@ -9,9 +9,7 @@ public class LunaBurnDriveStats extends BaseShipSystemScript {
         @Override
 	public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
             stats.getShieldDamageTakenMult().modifyMult(id, 1f - .9f * effectLevel);
-		
-		stats.getShieldUpkeepMult().modifyMult(id, 0f);
-           
+            stats.getShieldUpkeepMult().modifyMult(id, 0f);
             if (state == ShipSystemStatsScript.State.OUT) {
 			stats.getMaxSpeed().unmodify(id); // to slow down ship to its regular top speed while powering drive down
 		} else {
@@ -57,9 +55,12 @@ public class LunaBurnDriveStats extends BaseShipSystemScript {
         @Override
 	public StatusData getStatusData(int index, State state, float effectLevel) {
 		if (index == 0) {
-			return new StatusData("shield absorbs 10x damage and incread engine power", false);
+			return new StatusData("shield absorbs 10x damage", false);
 			//return new StatusData("increased engine power", false);
 		}
+                if (index == 1) {
+                        return new StatusData("increased engine power", false);
+                }
 		//		else if (index == 1) {
 //			return new StatusData("shield upkeep reduced to 0", false);
 //		} else if (index == 2) {
